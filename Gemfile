a0c20bb6264e4e7628d2e4f221e4bc8171eaab96
jvaleroliet <olietvalero@gmail.com>
2025-01-06 09:26:35 +0100

    copy nofuture

--- Gemfile
+++ Gemfile
@@ -1,13 +1,15 @@
-source 'https://rubygems.org'
+# frozen_string_literal: true
+
+source "https://rubygems.org"
 require 'rubygems'
 require 'json'
 
+# gem "rails"
 gem 'jekyll'
 gem 'webrick'
 gem 'jekyll-postcss'
 
-
 group :jekyll_plugins do
     gem 'jekyll_picture_tag', '~> 2.0'
-
+    
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,25 @@ module.exports = {
       './_drafts/**/*.html',
       './_includes/**/*.html',
       './_layouts/**/*.html',
+      './_releases/**/*.html',
+      './_dates/**/*.html',
       './_posts/*.md',
       './*.md',
       './*.html',
     ],
     theme: {
-      theme: {
-        extend: {},
-      },
+        extend: {
+          colors: {
+            'base1':'#B14A7A',
+            'base2':'#864E68',
+            'base3':'#DB3682',
+            'base4':'#5C4550'
+          },
+          fontFamily: {
+            'karrik' : ['karrik', 'sans-serif'],  
+          }
+        },
     },
     plugins: []
   }
-  
+
